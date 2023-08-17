39a13df4b781f286ff05cacd67cfc6f507869f61
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-17 10:11:36 +0530

    coupon  application

---
+++ coupon-app/coupon-app/src/main/java/com/coupon/application/service/CouponAppApplication.java
@@ -0,0 +1,7 @@
+package com.coupon.application.service;
+
+import org.springframework.boot.ApplicationArguments;
+
+public interface CouponAppApplication {
+    void run(ApplicationArguments args) throws Exception;
+}
