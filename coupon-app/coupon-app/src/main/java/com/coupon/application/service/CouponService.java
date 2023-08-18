31af1622cbc328de6cda83d962658fec08a1be0a
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-18 09:03:21 +0530

    coupon service

---
+++ coupon-app/coupon-app/src/main/java/com/coupon/application/service/CouponService.java
@@ -0,0 +1,45 @@
+package com.coupon.application.service;
+
+import com.coupon.application.helper.CouponHelper;
+import com.coupon.application.model.Coupon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@Component
+public class CouponService {
+
+    @Autowired
+    private CouponHelper couponHelper;
+
+    public CouponService(CouponHelper couponHelper){
+        this.couponHelper = couponHelper;
+    }
+
+    public String newCoupon(){
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+    public List<String> getNewCoupon(int quantity){
+        List<String> coupons = new ArrayList<>();
+        for(int i = 0; i < quantity; i++){
+            String newCoupon = String.valueOf(newCoupon());
+            coupons.add(newCoupon);
+        }
+        return coupons;
+
+    }
+
+    public Coupon generateNewCoupon() {
+        Coupon coupon = Coupon.builder()
+                .id(UUID.randomUUID().toString())
+                .validFor(5 + new Random().nextInt(90))
+                .type("COUPON")
+                .build();
+        return coupon;
+    }
+}
