9a2396dc053a20d2ea07361d347be40bc1f6767e
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-22 23:04:54 +0530

    user

--- coupon_app/src/main/java/com/coupon/application/model/User.java
+++ coupon_app/src/main/java/com/coupon/application/model/User.java
@@ -1,14 +1,30 @@
 package com.coupon.application.model;
-
+import jakarta.persistence.*;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
+@Table(name = "app_user")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name =  "email_id")
     private String emailId;
+
+    @Column(name = "phone")
     private long phone;
 }
