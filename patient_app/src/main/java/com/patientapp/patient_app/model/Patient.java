347c8bdffd809755094f9240dbdda4e6406578d7
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-22 23:06:15 +0530

    patient

--- patient_app/src/main/java/com/patientapp/patient_app/model/Patient.java
+++ patient_app/src/main/java/com/patientapp/patient_app/model/Patient.java
@@ -1,13 +1,28 @@
 package com.patientapp.patient_app.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Entity
 public class Patient {
+    @Column(name = "Patient_name")
     private String name;
+
+    @Column(name = "disease")
     private String disease;
+
+    @Column(name = "phone")
     private int phone;
+
+    @Column(name = "age")
     private int age;
+    @Id
+    @GeneratedValue
+    private int id;
 }
