ed3afdab8e4f002460d626bc0f40a399d7ddab36
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-22 23:06:39 +0530

    patient application

--- patient_app/src/main/java/com/patientapp/patient_app/PatientAppApplication.java
+++ patient_app/src/main/java/com/patientapp/patient_app/PatientAppApplication.java
@@ -1,13 +1,27 @@
 package com.patientapp.patient_app;
 
+import com.patientapp.patient_app.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientAppApplication {
+
+public class PatientAppApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientAppApplication.class, args);
 	}
+	@Autowired
+	private PatientService patientService;
+
+	@Override
+	public void run(ApplicationArguments args) throws  Exception{
+		String s = patientService.getNewPatient();
+		System.out.println("New Patient = "+ s);
+	}
+
 
 }
