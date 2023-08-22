f60ba2e01cd7e3cdb21305889defe582bdc0a3c3
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-22 23:05:26 +0530

    user repository

---
+++ coupon_app/src/main/java/com/coupon/application/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.coupon.application.repository;
+
+import com.coupon.application.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository {
+
+    //void save(User user);
+}
