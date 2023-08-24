6b44d785d2d9aac26e28dab3a73c3249ff7051d3
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-24 15:36:55 +0530

    coupon

--- coupon_app/src/main/java/com/coupon/application/model/Coupon.java
+++ coupon_app/src/main/java/com/coupon/application/model/Coupon.java
@@ -1,15 +1,46 @@
 package com.coupon.application.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Data
 @Builder
+@Table(name = "coupon_table")
+@Entity
 public class Coupon {
-    private String id;
-    private String type;
-    private int validFor;
+    @Id
+    @GeneratedValue
+    private int id;
 
+   // @Column(name = "title")
+    private String title;
+
+    //@Column(name = "description")
+    private String description;
+
+    @Column(name = "coupon_code")
+    private  String couponcode;
+
+   // @Column(name = "discount")
+    private int discount;
+
+    //@Column(name = "category")
+    private String category;
+
+    @Column(name = "created_date")
+    private LocalDate createddate;
+
+    @Column(name = "validity_date")
+    private LocalDate validitydate;
+
+   @Column(name = "is_Active")
+    private boolean isActive;
+
+    @Column(name = "created_by")
+    private String createdBy;
+   private String type;
 }
