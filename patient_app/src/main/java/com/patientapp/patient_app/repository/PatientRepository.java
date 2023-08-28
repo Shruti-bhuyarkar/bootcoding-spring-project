cdafa42e5f31f969e8d679417ee55eabf015e0a3
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-08-28 18:35:13 +0530

    patient repository

--- patient_app/src/main/java/com/patientapp/patient_app/repository/PatientRepository.java
+++ patient_app/src/main/java/com/patientapp/patient_app/repository/PatientRepository.java
@@ -4,6 +4,6 @@ import com.patientapp.patient_app.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface PatientRepository extends JpaRepository<Patient,Integer> {
 }
