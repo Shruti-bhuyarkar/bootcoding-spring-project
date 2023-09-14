e57670119dc224b4aaaf0de6cf3f1afdd63b83dc
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-15 01:13:52 +0530

    hire date generator

---
+++ employee_app/src/main/java/com/employee/employee_app/utils/HireDateGenerator.java
@@ -0,0 +1,28 @@
+package com.employee.employee_app.utils;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class HireDateGenerator {
+    public static Date randomDate() {
+        long aDay = TimeUnit.DAYS.toMillis(1); // 1 day
+        long now = new Date().getTime(); // current time
+        Date yesterday = new Date(now - aDay);
+        Date aYearAgo = new Date(now - aDay * 365);
+        return between(aYearAgo, yesterday);
+    }
+    public static Date between(Date startInclusive, Date endExclusive) {
+        Random random = new Random();
+
+        // Generate a random timestamp between 946684800000 (2000-01-01) and 1672220400000 (2023-12-31)
+        long start = startInclusive.getTime();
+        long end = endExclusive.getTime();
+        long randomTimestamp = ThreadLocalRandom
+                .current()
+                .nextLong(start,end);
+
+        return new Date(randomTimestamp);
+    }
+}
